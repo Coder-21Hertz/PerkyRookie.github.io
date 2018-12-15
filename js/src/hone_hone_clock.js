c61980845225a5d02791345254942dbb1012a129
{PerkyRookie} <{943886769@qq.com}>
2018-12-15 19:12:29 +0800

    Site updated: 2018-12-15 19:12:27

--- js/src/hone_hone_clock.js
+++ js/src/hone_hone_clock.js
@@ -3,7 +3,7 @@
   /* code */
   var initClock = function(){
     var sHtml = '';
-    sHtml += '<div style="position: fixed;right: 10px;top: 20px;width: 160px;height: 70px;">';
+    sHtml += '<div style="position: fixed;right: 30px;top: 25px;width: 160px;height: 70px;">';
     sHtml += '  <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="160" height="70" id="honehoneclock" align="middle">';
     sHtml += '    <param name="allowScriptAccess" value="always">';
     sHtml += '    <param name="movie" value="http://chabudai.sakura.ne.jp/blogparts/honehoneclock/honehone_clock_tr.swf">';
